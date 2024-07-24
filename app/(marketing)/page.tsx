653ee77c40910b11eababc232afc3c6f4b50c7b5
zenik-kun <nikirukyun@gmail.com>
2024-07-24 17:34:24 +0530

    initial

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+
+
+const MarketingPage = () => {
+  return (
+    <div className = "min-h-full flex flex-col">
+      <div className = "flex flex-col items-center justify-center md:justify-start text-center gap-y-8">
+
+      </div>
+    </div>
+  );
+}
+
+export default MarketingPage;
