25f9e1a6bbf55e7d50fc549ab0a33d418a6bf8f6
zenik-kun <nikirukyun@gmail.com>
2024-07-25 20:20:50 +0530

    landing page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,12 +11,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        media: "(prefers-colors-scheme: light)",
+        media: "(prefers-color-scheme: light)",
         url: "/logo.svg",
         href: "/logo.svg",
       },
       {
-        media: "(prefers-colors-scheme: dark)",
+        media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
         href: "/logo-dark.svg",
       },
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang = "en" suppressHydrationWarning>
+      <body className = {inter.className}>
+        <ThemeProvider
+          attribute = "class"
+          defaultTheme = "system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey = "notika-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
